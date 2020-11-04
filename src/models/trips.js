ecd712199810a3b02c883d55c51a155089da81e3
Lalit Sharma <dev.lalit.sh@gmail.com>
2020-11-05 00:34:11 +0530

    trip creation fixed

--- src/models/trips.js
+++ src/models/trips.js
@@ -4,7 +4,6 @@ import mongoose, { Schema } from "mongoose";
 const schema = new Schema({
     name: {
         type: String,
-        unique: [true, "Another trip with same name already exsit"],
         required: [true, "Trip name is required"]
     },
     username: {
@@ -14,10 +13,11 @@ const schema = new Schema({
     members: [String],
     isActive: {
         type: Boolean,
-        unique: [true, "Only one trip can stay active at one time"],
         default: false
     }
 }, { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } });
+
+schema.index({ name: 1, username: 1 }, { unique: true });
 
 const model = mongoose.model("Trips", schema);
 export default model;
